c6b6b03f499cbcd8ec305321ff35af2463220ccd
Bat <baptiste@gelez.xyz>
2018-03-17 17:06:58 +0100

    Add a site description

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-config[:description] = ''
+config[:description] = 'Unixcorn.org, site web de licornes fanatiques. Hébergement et services web à prix libre mais non gratuit. Le tout propulsé par des technologies libres et respectueuses de votre vie privée.'
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
