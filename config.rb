57f334154e385aeb69f39f25bb53f7bde2f9c6d5
Bat <baptiste@gelez.xyz>
2018-03-17 21:35:16 +0100

    Services

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 config[:description] = 'Unixcorn.org, site web de licornes fanatiques. Hébergement et services web à prix libre mais non gratuit. Le tout propulsé par des technologies libres et respectueuses de votre vie privée.'
 
+activate :directory_indexes
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
