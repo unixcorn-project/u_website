2902c024bdf0b6fac94530ed02a476b59bcb0f59
Bat <baptiste@gelez.xyz>
2018-03-18 14:38:20 +0100

    Setup a blog

--- config.rb
+++ config.rb
@@ -1,7 +1,5 @@
 config[:description] = 'Unixcorn.org, site web de licornes fanatiques. Hébergement et services web à prix libre mais non gratuit. Le tout propulsé par des technologies libres et respectueuses de votre vie privée.'
 
-activate :directory_indexes
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -10,3 +8,14 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 end
+
+set :markdown_engine, :kramdown
+
+activate :blog do |blog|
+  blog.sources = "blog/{year}-{month}-{day}-{title}.html"
+  blog.permalink = "blog/{year}/{title}.html"
+
+  blog.layout = "blog_layout"
+end
+
+activate :directory_indexes
